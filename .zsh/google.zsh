a840f1ee1ea6a347c6fc2074f17e14addc92815b
jessesomerville <jssomerville2@gmail.com>
2021-07-16 22:28:05 -0400

    Cleaned up Google configs and removed old files

---
+++ .zsh/google.zsh
@@ -0,0 +1,18 @@
+# Google autocompletion for hgd (Fig)
+source /etc/bash_completion.d/hgd
+
+# fzf autocompletion
+source /usr/share/doc/fzf/examples/key-bindings.zsh
+source /usr/share/doc/fzf/examples/completion.zsh
+
+# Function tmux uses to change title
+function tmux_title() {
+    if [[ $PWD =~ /google/src/cloud/[^/]+/([^/]+)/.* ]]; then
+        tmx2 rename-window "${match[1]}"
+    else
+        tmx2 rename-window "$(basename "`pwd`")"
+    fi
+}
+if [[ ! -z "$TMUX" ]]; then
+    precmd_functions+=(tmux_title)
+fi
