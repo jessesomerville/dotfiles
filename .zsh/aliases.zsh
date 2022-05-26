1155dc5c15ee49674ba7f7807714f3256e9b7e07
jessesomerville <jssomerville2@gmail.com>
2022-05-26 18:14:29 -0400

    pre chezmoi

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -7,7 +7,7 @@ alias cat="bat"
 alias ccat="command cat"
 alias cdgo="cd $(go env GOPATH)/src/github.com/jessesomerville"
 alias config="/usr/bin/git --git-dir=${HOME}/.cfg --work-tree=${HOME}"
-alias history="history -fD 0"  # Show history with time, date, and command runtime
+alias hist="history -fD 0"  # Show history with time, date, and command runtime
 alias la="lsd -lAh"
 alias ls="lsd"
 alias randstr='print -- $(tr -dc A-Za-z0-9 </dev/urandom | head -c 64)'
@@ -16,3 +16,4 @@ alias top="btm -b"
 alias vim="nvim"
 alias lines="echo ${(l:80::─:)}"
 alias envs="env | sort"
+
